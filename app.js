45970f0a0be9793e1e55fe8aa78156c187cd0c37
jesusj <jajm84@gmail.com>
2022-07-10 22:15:36 -0300

    agregue mas imagenes y estilos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const infoProductos = [
     { id: 1, name: "CUADRIL", price: 1180, stock: 10, img:"cuadril.jpg"},
     { id: 2, name: "PECETO", price: 1350, stock: 12, img:"peceto.jpg"},
-    { id: 3, name: "COSTILLA", price: 1080, stock: 20, img:"costilla.jpg" },
+    { id: 3, name: "COSTILLA", price: 1080, stock: 20, img:"costilla.jpg"},
+    { id: 4, name: "BIFE DE CHORIZO", price: 1400, stock: 20, img:"bife_de_chorizo.jpg"},
+    { id: 5, name: "COLITA DE CUADRIL", price: 1200, stock: 20, img:"colita-cuadril.jpg"},
+    { id: 6, name: "COSTELETA ESPECIAL", price: 950, stock: 25, img:"costeleta-especial.jpg"},
+    { id: 7, name: "TAPA DE CUADRIL", price: 1250, stock: 28, img:"tapa-de-cuadril.jpg"},
+    { id: 8, name: "TOMAHAWK", price: 1450, stock: 23, img:"tomahawk.jpg"},
+    { id: 9, name: "VACIO", price: 1180, stock: 16, img:"vacio.jpg"}
     
   ];
+
+  let carrito = [];
 
   const input = document.querySelector("input");
   input.addEventListener("input", () => {
@@ -17,36 +25,42 @@ const infoProductos = [
 
 
   const info = document.getElementById("info");
-  info.classList.add("row", "row-cols-1", "row-cols-md-3", "g-4");
+  info.classList.add("row", "row-cols-sm-2", "row-cols-md-3", "g-4");
 
   infoProductos.forEach((corte) => {
     const row = document.createElement("div");
     row.classList.add("col");
     row.innerHTML = `
-      <div class="card h-100" id="item">
+      <div class="card h-100 m-2" id="item">
         <img src="img/${corte.img}" class="card-img-top" id="cardimg" alt="...">
-        <div class="card-body">
+        <div class="card-body bg-black text-bg-primary rounded">
           <h5 class="card-title">${corte.name}</h5>
           <p class="card-text">Precio = $${corte.price} el Kilo</p>
           <p class="card-text">Stock = ${corte.stock}</p>
-          <a href="#" class="btn btn-primary" id="${corte.id}">Comprar</a>
+          <a href="#" class="btn btn-primary bg-danger" id="${corte.id}">Comprar</a>
         </div>
       </div>`;
   
     info.appendChild(row);
-
+    
   })
+
 
   info.addEventListener("click", e =>{
-    clickUsuario(e);
-  })
+        clickUsuario(e);
+      })
 
   const clickUsuario = e => {
-    const boton = e.target.classList.contains("btn")
-    console.log(boton);
-    if(boton === true){
-      console.log("usted realizo una compra");
-    }
+    const boton = e.target
+    const item = boton.classList.contains("btn");
+      console.log(boton);
+      
+      
+    
+    // console.log(boton);
+    // if(boton === true){
+    //   console.log("usted realizo una compra");
+    // }
   }
 
 
